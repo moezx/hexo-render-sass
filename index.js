e93fa134d000a8b53d4eac1872c8ab6a77d9011d
ポール ウェッブ <netopwibby@thenetwork.email>
2018-04-25 13:19:47 -0500

    Repo update

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-/* jshint undef: true, esversion: 6, -W097 */
-/* global require, hexo */
+/* global hexo */ "use strict";
 
 
 
-"use strict";
+//  P A C K A G E
 
 const renderer = require("./lib/renderer");
+
+
+
+//  E X P O R T S
 
 hexo.extend.renderer.register("scss", "css", renderer);
 hexo.extend.renderer.register("sass", "css", renderer);
